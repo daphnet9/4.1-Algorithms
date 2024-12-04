6e820bfafdfa831aabb66daf06ca169b886dc6e5
daphnet9 <daphnet9@nycstudents.net>
2024-12-04 21:15:18 +0000

    min & max & sum

--- Algorithms.java
+++ Algorithms.java
@@ -11,16 +11,16 @@ public class Algorithms {
         int evens = evens();
         int twos = twos();
         int greaterThan = greaterThan();
-        // int greatest = greatest();
-        // int least = least();
-        // int sum = sum();
+        int max = max();
+        int min = min();
+        int sum = sum();
         System.out.println(odds);
         System.out.println(evens);
         System.out.println(twos);
         System.out.println(greaterThan);
-        // System.out.println(greatest);
-        // System.out.println(least);
-        // System.out.println(sum);
+        System.out.println(max);
+        System.out.println(min);
+        System.out.println(sum);
         s.close();
     }
 
@@ -48,7 +48,8 @@ public class Algorithms {
         s = new Scanner(f);
         int twos = 0;
         while (s.hasNext()) {
-            if (s.nextInt() == 2)
+            int num = s.nextInt();
+            if (num >= 10 && num <= 99)
                 twos++;
         }
         return twos;
@@ -58,9 +59,42 @@ public class Algorithms {
         s = new Scanner(f);
         int greaterThan = 0;
         while (s.hasNext()) {
-            if (s.nextInt() >= 500)
+            if (s.nextInt() > 500)
                 greaterThan++;
         }
         return greaterThan;
+    }
+
+    public static int max() throws FileNotFoundException {
+        s = new Scanner(f);
+        int max = Integer.MIN_VALUE;
+        while (s.hasNext()) {
+            int num = s.nextInt();
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    public static int min() throws FileNotFoundException {
+        s = new Scanner(f);
+        int min = Integer.MAX_VALUE;
+        while (s.hasNext()) {
+            int num = s.nextInt();
+            if (num < min) {
+                min = num;
+            }
+        }
+        return min;
+    }
+
+    public static int sum() throws FileNotFoundException {
+        s = new Scanner(f);
+        int sum = 0;
+        while (s.hasNext()) {
+            sum += s.nextInt();
+        }
+        return sum;
     }
 }
